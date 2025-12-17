cfb8bdc873334a1193e27e9807110870041d4ada
Shizu-99 <flew1440@gmail.com>
2025-12-17 19:16:58 +1300

    Remove xml struct and Add database struct

--- models/models.go
+++ models/models.go
@@ -1,27 +1,8 @@
 package models
 
-import (
-	"encoding/xml"
-)
-
-type Archers struct {
-	XMLName xml.Name `xml:"archers"`
-	Archers []Archer `xml:"archer"`
-}
-
 type Archer struct {
-	XMLName xml.Name `xml:"archer"`
-	Id      int      `xml:"id,attr"`
-	Name    string   `xml:"name"`
-	School  string   `xml:"school"`
-	Team    string   `xml:"team"`
-	Score   Score    `xml:"score"`
-}
-
-type Score struct {
-	XMLName xml.Name `xml:"score"`
-	Num_X   int      `xml:"num_x,attr"`
-	Num_10  int      `xml:"num_10,attr"`
-	Hits    int      `xml:"hits,attr"`
-	Total   int      `xml:"total"`
+	Name    string `db:"name"`
+	School  string `db:"school"`
+	Bowtype string `db:"bowtype"`
+	Age     int    `db:"age"`
 }
