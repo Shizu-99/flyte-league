43c8cd95a72df96e3f7edd68c25deec3ba84fcdc
Shizu-99 <flew1440@gmail.com>
2025-12-17 19:12:33 +1300

    Add initial setup for database

---
+++ database/database.go
@@ -0,0 +1,37 @@
+package database
+
+import (
+	"github.com/jmoiron/sqlx"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var db *sqlx.DB
+
+const SCHEMA = `
+PRAGMA journal_mode = WAL;
+PRAGMA busy_timeout = 5000;
+
+CREATE TABLE IF NOT EXISTS archers (
+	archer_id INTEGER PRIMARY KEY,
+	name TEXT NOT NULL COLLATE NOCASE,
+	school TEXT,
+	bowtype TEXT NOT NULL,
+	age INTEGER NOT NULL
+);
+`
+
+func OpenDatabase(dbPath string) error {
+	var err error
+	db, err = sqlx.Open("sqlite3", dbPath)
+	if err != nil {
+		return err
+	}
+	db.MustExec(SCHEMA)
+	return nil
+}
+
+func CloseDatabase() {
+	if db != nil {
+		db.Close()
+	}
+}
