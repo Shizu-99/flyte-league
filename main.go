a14d800770600a26bfe4851996d93d7daa4555f5
Shizu-99 <flew1440@gmail.com>
2025-12-18 14:55:29 +1300

    Add database connection

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"flyte-league/database"
+	"log"
+)
 
+func main() {
+	if err := database.OpenDatabase("./flyte.sqlite3"); err != nil {
+		log.Fatal(err)
+	}
+	defer database.CloseDatabase()
 }
