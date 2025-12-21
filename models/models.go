ad7ef1e67f2222cfe4da3193792fca959184b6ac
Shizu-99 <flew1440@gmail.com>
2025-12-21 16:53:58 +1300

    Add struct for round, score and team

--- models/models.go
+++ models/models.go
@@ -1,9 +1,34 @@
 package models
 
+import "time"
+
 type Archer struct {
-	Firstname string `db:"firstname"`
-	Lastname  string `db:"lastname"`
+	First_name string `db:"first_name"`
+	Last_name  string `db:"last_name"`
+	School     string `db:"school"`
+	Bowtype    string `db:"bowtype"`
+	Age        int    `db:"age"`
+}
+
+type Round struct {
+	Round_num  int       `db:"round_num"`
+	Session    int       `db:"session"`
+	Date       time.Time `db:"date"`
+	Event_name string    `db:"event_name"`
+}
+
+type Score struct {
+	Archer Archer `db:"archer"`
+	Event  string `db:"event"`
+	Score  int    `db:"score"`
+	Xs     int    `db:"xs"`
+	tens   int    `db:"tens"`
+	hits   int    `db:"hits"`
+}
+
+type Team struct {
+	Team_name string `db:"team_name"`
 	School    string `db:"school"`
+	Division  int    `db:"division"`
 	Bowtype   string `db:"bowtype"`
-	Age       int    `db:"age"`
 }
