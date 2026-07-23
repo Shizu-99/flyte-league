2f5d34f69086501d8072669ca43c3138be41822f
Shizu-99 <flew1440@gmail.com>
2026-07-23 22:12:49 +1200

    Alter models to refelect new database schema.

--- models/models.go
+++ models/models.go
@@ -2,33 +2,53 @@ package models
 
 import "time"
 
+type School struct {
+	ID       int    `db:"school_id"`
+	Name     string `db:"name"`
+	Location string `db:"location"`
+}
+
 type Archer struct {
+	ID         int    `db:"archer_id"`
 	First_name string `db:"first_name"`
 	Last_name  string `db:"last_name"`
-	School     string `db:"school"`
-	Bowtype    string `db:"bowtype"`
-	Age        int    `db:"age"`
 }
 
 type Event struct {
-	Event_name string    `db:"event_name"`
-	Date       time.Time `db:"date"`
-	Round_num  int       `db:"round_num"`
-	Session    int       `db:"session"`
+	Name        string    `db:"name"`
+	Description string    `db:"description"`
+	Start_date  time.Time `db:"start_date"`
+	End_date    time.Time `db:"end_date"`
+}
+
+type EventDay struct {
+	Date       time.Time `db:"day_date"`
+	Day_number int       `db:"day_number"`
+}
+
+type Round struct {
+	Name               string `db:"name"`
+	Max_possible_score int    `db:"max_possible_score"`
+}
+
+type Session struct {
+	Name       string    `db:"name"`
+	Start_time time.Time `db:"start_time"`
 }
 
 type Score struct {
-	Archer Archer `db:"archer"`
-	Event  Event  `db:"event"`
-	Score  int    `db:"score"`
-	Xs     int    `db:"xs"`
-	tens   int    `db:"tens"`
-	hits   int    `db:"hits"`
+	Score int `db:"score"`
+	Xs    int `db:"xs_count"`
+	Tens  int `db:"tens_count"`
+	hits  int `db:"hits"`
 }
 
 type Team struct {
-	Team_name string `db:"team_name"`
-	School    string `db:"school"`
+	Team_name string `db:"name"`
 	Division  int    `db:"division"`
-	Bowtype   string `db:"bowtype"`
+}
+
+type Registration struct {
+	Division string `db:"division"`
+	Bowtype  string `db:"bow_type"`
 }
