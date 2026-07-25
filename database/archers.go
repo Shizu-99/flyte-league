562a7dcc4236c8f0039fb0e93d08a5429240b618
Shizu-99 <flew1440@gmail.com>
2026-07-25 15:39:22 +1200

    Remove functions that relate to columns no longer in the archers table.

--- database/archers.go
+++ database/archers.go
@@ -35,15 +35,6 @@ func DBGetArcherByFullName(first_name string, last_name string) (*models.Archer,
 	return archer, nil
 }
 
-func DBGetAllArchersInSchool(school string) ([]models.Archer, error) {
-	archers := []models.Archer{}
-	err := db.Select(archers, `SELECT first_name, last_name, school, bowtype, age FROM archers WHERE school=$1 ORDER BY last_name ASC`, school)
-	if err != nil {
-		return nil, err
-	}
-	return archers, nil
-}
-
 func DBGetArchersByfirst_name(name string) ([]models.Archer, error) {
 	archers := []models.Archer{}
 	err := db.Select(archers, `SELECT first_name, last_name, school, bowtype, age FROM archers WHERE first_name=$1 ORDER BY first_name ASC`, name)
@@ -56,24 +47,6 @@ func DBGetArchersByfirst_name(name string) ([]models.Archer, error) {
 func DBGetArchersBylast_name(name string) ([]models.Archer, error) {
 	archers := []models.Archer{}
 	err := db.Select(archers, `SELECT first_name, last_name, school, bowtype, age FROM archers WHERE last_name=$1 ORDER BY last_name ASC`, name)
-	if err != nil {
-		return nil, err
-	}
-	return archers, nil
-}
-
-func DBGetAllArchersByBowtype(bowtype string) ([]models.Archer, error) {
-	archers := []models.Archer{}
-	err := db.Select(archers, `SELECT first_name, last_name, school, bowtype, age FRROM archers WHERE bowtype=$1 ORDER BY last_name ASC`, bowtype)
-	if err != nil {
-		return nil, err
-	}
-	return archers, nil
-}
-
-func DBGetAllArchersByAge(upperAgeLimit int, lowerAgeLimit int) ([]models.Archer, error) {
-	archers := []models.Archer{}
-	err := db.Select(archers, `SELECT first_name, last_name, school, bowtype, age FRROM archers WHERE age<$1 AND age >$2 ORDER BY last_name ASC`, upperAgeLimit, lowerAgeLimit)
 	if err != nil {
 		return nil, err
 	}
