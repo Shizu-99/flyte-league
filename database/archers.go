e9d435b50c4b27d29ebfc2b090ae99ed5dc4561b
Shizu-99 <flew1440@gmail.com>
2026-08-16 12:40:10 +1200

    Remove individual insertion function and change DBInsertMultipleArchers to DBInsertArchers. Revert change to pointer types in some functions. Add docstring to DBGetArchersBy(First/Last)Name and add testing for no rows returned in those functioins.

--- database/archers.go
+++ database/archers.go
@@ -1,16 +1,11 @@
 package database
 
-import "flyte-league/models"
+import (
+	"database/sql"
+	"flyte-league/models"
+)
 
-func DBInsertArcher(archer *models.Archer) error {
-	_, err := db.NamedExec(`INSERT OR IGNORE INTO archers (first_name, last_name) VALUES (:first_name, :last_name)`, archer)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func DBInsertMultipleArchers(archers []models.Archer) error {
+func DBInsertArchers(archers []*models.Archer) error {
 	_, err := db.NamedExec(`INSERT OR IGNORE INTO archers (first_name, last_name) VALUES (:first_name, :last_name)`, archers)
 	if err != nil {
 		return err
@@ -18,29 +13,51 @@ func DBInsertMultipleArchers(archers []models.Archer) error {
 	return nil
 }
 
-func DBGetArcherByFullName(first_name string, last_name string) (models.Archer, error) {
-	archer := models.Archer{}
+func DBGetArcherByFullName(first_name string, last_name string) (*models.Archer, error) {
+	archer := &models.Archer{}
 	err := db.Get(archer, `SELECT first_name, last_name FROM archers WHERE first_name=$1 AND last_name=$2`, first_name, last_name)
 	if err != nil {
-		return archer, err
+		return nil, err
 	}
 	return archer, nil
 }
 
+// DBGetArchersByFirstName returns a slice of Archer structs
+// from the database. The function returns all archers
+// whose first name starts with the given substring.
+//
+// If there are no archers whose first name starts with a given substring
+// the function returns sql.ErrNoRows.
 func DBGetArchersByFirstName(name string) ([]models.Archer, error) {
 	archers := []models.Archer{}
-	err := db.Select(archers, `SELECT first_name, last_name FROM archers WHERE first_name LIKE '$1%' ORDER BY first_name ASC`, name)
+	query := `SELECT first_name, last_name FROM archers WHERE first_name LIKE ? ORDER BY first_name ASC`
+	searchPattern := name + "%"
+	err := db.Select(&archers, query, searchPattern)
 	if err != nil {
 		return nil, err
+	}
+	if len(archers) == 0 {
+		return nil, sql.ErrNoRows
 	}
 	return archers, nil
 }
 
-func DBGetArchersBylast_name(name string) ([]models.Archer, error) {
+// DBGetArchersByLastName returns a slice of Archer structs
+// from the database. The function returns all archers
+// whose last name starts with the given substring.
+//
+// If there are no archers whose last name starts with a given substring
+// the function returns sql.ErrNoRows.
+func DBGetArchersByLastName(name string) ([]models.Archer, error) {
 	archers := []models.Archer{}
-	err := db.Select(archers, `SELECT first_name, last_name FROM archers WHERE last_name LIKE '$1%' ORDER BY last_name ASC`, name)
+	query := `SELECT first_name, last_name FROM archers WHERE last_name LIKE ? ORDER BY last_name ASC`
+	searchPattern := name + "%"
+	err := db.Select(&archers, query, searchPattern)
 	if err != nil {
 		return nil, err
+	}
+	if len(archers) == 0 {
+		return nil, sql.ErrNoRows
 	}
 	return archers, nil
 }
