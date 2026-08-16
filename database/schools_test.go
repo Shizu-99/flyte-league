d4f788a8d9ae9fc2771759f0a8e458824d7b4e55
Shizu-99 <flew1440@gmail.com>
2026-08-16 13:57:08 +1200

    Change TestDBInsertSchools to properly test insertion behaviour and UNIQUE constraints.

--- database/schools_test.go
+++ database/schools_test.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"database/sql"
 	"testing"
 
 	"flyte-league/models"
@@ -8,19 +9,109 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestDBInsertSchool(t *testing.T) {
-	school := models.School{
-		ID:       1,
-		Name:     "Motosu High School",
-		Location: "Japan",
+func TestDBInsertSchools(t *testing.T) {
+	schools := []*models.School{
+		{
+			Name: "Motosu High School",
+			Location: sql.NullString{
+				String: "Japan",
+				Valid:  true,
+			},
+		},
+		{
+			Name: "Maehiba Graduate School of Science",
+			Location: sql.NullString{
+				String: "Japan",
+				Valid:  true,
+			},
+		},
+		{
+			Name: "Kunugigaoka Junior High School",
+			Location: sql.NullString{
+				String: "Japan",
+				Valid:  true,
+			},
+		},
+		{
+			Name: "Motosu High School",
+			Location: sql.NullString{
+				String: "",
+				Valid:  false,
+			},
+		},
+	}
+	expectedSchools := []models.School{
+		{
+			Name: "Motosu High School",
+			Location: sql.NullString{
+				String: "Japan",
+				Valid:  true,
+			},
+		},
+		{
+			Name: "Maehiba Graduate School of Science",
+			Location: sql.NullString{
+				String: "Japan",
+				Valid:  true,
+			},
+		},
+		{
+			Name: "Kunugigaoka Junior High School",
+			Location: sql.NullString{
+				String: "Japan",
+				Valid:  true,
+			},
+		},
+		{
+			Name: "Motosu High School",
+			Location: sql.NullString{
+				String: "",
+				Valid:  false,
+			},
+		},
+		{
+			Name: "Raelion Academy",
+			Location: sql.NullString{
+				String: "Elydes",
+				Valid:  true,
+			},
+		},
 	}
 	tests := []struct {
 		name           string
-		schoolToInsert *models.School
+		schoolToInsert []*models.School
+		expected       []models.School
+		expectErr      bool
+		expectedErr    error
 	}{
 		{
-			name:           "Successful Insertion",
-			schoolToInsert: &school,
+			name: "Successful Insertion",
+			schoolToInsert: []*models.School{
+				{
+					Name: "Raelion Academy",
+					Location: sql.NullString{
+						String: "Elydes",
+						Valid:  true,
+					},
+				},
+			},
+			expected:    expectedSchools,
+			expectErr:   false,
+			expectedErr: nil,
+		},
+		{
+			name:           "Non-unique name and location",
+			schoolToInsert: schools[:1],
+			expected:       nil,
+			expectErr:      true,
+			expectedErr:    nil,
+		},
+		{
+			name:           "Non-unique name with NULL location",
+			schoolToInsert: schools[3:],
+			expected:       nil,
+			expectErr:      true,
+			expectedErr:    nil,
 		},
 	}
 
@@ -31,15 +122,29 @@ func TestDBInsertSchool(t *testing.T) {
 			}
 			defer CloseDatabase()
 
-			err := DBInsertSchool(test.schoolToInsert)
+			err := DBInsertSchools(schools)
 			if err != nil {
 				t.Error(err)
 			}
-			actualSchool, err := DBGetSchoolByName("Motosu High School")
-			if err != nil {
-				t.Error(err)
+
+			err = DBInsertSchools(test.schoolToInsert)
+			if test.expectErr {
+				if assert.Error(t, err) {
+					if test.expectedErr != nil {
+						assert.ErrorIs(t, err, test.expectedErr)
+					} else {
+						assert.ErrorContains(t, err, "UNIQUE constraint failed:")
+					}
+				}
+			} else {
+				if assert.NoError(t, err) {
+					actualSchools, err := DBGetAllSchools()
+					if err != nil {
+						t.Error(err)
+					}
+					assert.ElementsMatch(t, expectedSchools, actualSchools)
+				}
 			}
-			assert.Equal(t, test.schoolToInsert, actualSchool)
 		})
 	}
 }
